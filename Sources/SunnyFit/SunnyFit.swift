0d665ad8e08e294a3a2d7d1ba20ecd1a9900e546
Alsey Coleman Miller <alseycmiller@gmail.com>
2024-09-07 23:40:11 -0400

    Add `SunnyFitAccessory`

--- Sources/SunnyFit/SunnyFit.swift
+++ Sources/SunnyFit/SunnyFit.swift
@@ -1,1 +1,41 @@
+import Foundation
+import Bluetooth
+import GATT
 
+public enum SunnyFitAccessory: Equatable, Hashable, Codable, Sendable {
+    
+    /// Mini Stepper
+    case miniStepper
+    
+    /// Total Body Smart Exercise Stepper Machine
+    case totalBodyStepper
+    
+    /// Smart Twist Stepper Machine
+    case twistStepper
+}
+
+public enum SunnyFitAccessoryType: String, Codable, CaseIterable, Sendable {
+    
+    /// Smart Mini Stepper
+    ///
+    ///
+    /// ![Image](https://sunnyhealthfitness.com/cdn/shop/files/sunny-health-fitness-steppers-smart-mini-stepper-w-exercise-bands-no012smart-1_1100x.jpg?v=1715123786)
+    ///
+    /// [Product Page](https://sunnyhealthfitness.com/products/smart-mini-stepper-with-exercise-bands-no-012smart)
+    case miniStepper                = "NO. 012 SMART"
+    
+    /// Total Body Smart Exercise Stepper Machine
+    ///
+    ///
+    /// ![Image](https://sunnyhealthfitness.com/cdn/shop/files/Sunny-health-fitness-Accessories-Multi-Purpose-Air-drive-Adjustable-Standing-Desk-SF-A023001-00_07ec88e8-1548-4c6f-af62-803979a620d5_1100x.jpg?v=1696268183)
+    ///
+    /// [Product Page](https://sunnyhealthfitness.com/products/total-body-smart-exercise-stepper-machine-sf-s0978smart)
+    case totalBodyStepper           = "SF-S0978SMART"
+    
+    /// Smart Twist Stepper Machine
+    ///
+    /// ![Image](https://sunnyhealthfitness.com/cdn/shop/files/sunny-health-fitness-steppers-smart-twist-stepper-machine-sf-s0979smart-1_1100x.jpg?v=1720034544)
+    ///
+    /// [Product Page](https://sunnyhealthfitness.com/products/smart-twist-stepper-machine-sf-s0979smart)
+    case twistStepper               = "SF-S0979SMART"
+}
